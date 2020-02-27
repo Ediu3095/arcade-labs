89ced5703d26816c56eec2f07409217ff12c2320
Ediu3095 <eduardoruizsabajanes@gmail.com>
2020-02-27 23:25:49 +0100

    Update lab03-animation.py

--- lab03-animation/lab03-animation.py
+++ lab03-animation/lab03-animation.py
@@ -10,9 +10,21 @@ class MyGame(arcade.Window):
         self.centreY = Cy
         self.speedX = Sx
         self.speedY = Sy
-        self.framerate = 60
+        self.framerate = [0 for i in range(30)]
         self.set_update_rate(1 / 60)
         arcade.set_background_color(arcade.color.BATTLESHIP_GREY)
+        self.f3 = 0
+
+    def averageFPS(self):
+        FPS = 0
+        for elem in self.framerate:
+            FPS += elem / len(self.framerate)
+        return int(FPS)
+
+    def FPShift(self, new_framerate):
+        for i in range(len(self.framerate) - 1):
+            self.framerate[i] = self.framerate[i + 1]
+        self.framerate[len(self.framerate) - 1] = new_framerate
 
     def on_draw(self):
         arcade.start_render()
@@ -62,10 +74,20 @@ class MyGame(arcade.Window):
                                     [self.centreX + 80, self.centreY + 80]], arcade.color.BLACK)
 
         arcade.draw_rectangle_filled(self.centreX, self.centreY + 90, 120, 20, arcade.color.BLACK)
-        arcade.draw_text(str(self.framerate), 0, self.height - 25, arcade.color.YELLOW_ORANGE, 20, bold=True)
+        if self.f3 != 0:
+            arcade.draw_text("FPS: " + str(self.averageFPS()), 2, self.height - 27, arcade.color.BLACK, 20, bold=True)
+            arcade.draw_text("FPS: " + str(self.averageFPS()), 0, self.height - 25, arcade.color.WHITE, 20, bold=False)
+            if self.f3 == 2:
+                arcade.draw_rectangle_filled(len(self.framerate) * 5, 60, len(self.framerate) * 10, 120,
+                                             arcade.color.WHITE)
+                arcade.draw_rectangle_filled(len(self.framerate) * 5, self.averageFPS() * 2, len(self.framerate) * 10,
+                                             1, arcade.color.RED)
+                for i in range(len(self.framerate)):
+                    arcade.draw_rectangle_filled(i * 10 + 5, self.framerate[i] * 2, 10, 1, arcade.color.BLACK)
 
     def on_update(self, delta_time):
-        self.framerate = 1 // delta_time
+        self.FPShift(int(1 // delta_time))
+        ''
         self.centreX = self.centreX + (self.speedX * delta_time)
         self.centreY = self.centreY + (self.speedY * delta_time)
         if self.centreX + 170 > self.width:
@@ -81,8 +103,14 @@ class MyGame(arcade.Window):
             self.centreY = 170
             self.speedY *= -1
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == arcade.key.F3:
+            self.f3 += 1
+            if self.f3 == 3:
+                self.f3 = 0
 
-Screen = MyGame(randrange(600, 1200), randrange(400, 800), "Dwarf Fortress logo", 300, 300, randrange(1000),
-                randrange(1000))
+
+Screen = MyGame(randrange(600, 1200), randrange(400, 800), "Dwarf Fortress logo", 300, 300, randrange(500),
+                randrange(500))
 
 arcade.run()
